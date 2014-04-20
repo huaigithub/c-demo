6b2aae5b40db371e892d8edffbeaa859967fe707
huaigithub <yang20125002@163.com>
2014-04-20 21:32:35 +0800

    add xxxxxx

---
+++ example00001/exp001.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 0;
+    char ch;
+    float fvalue;
+    char *str = NULL;
+    scanf("%d",&a);
+    scanf("%s",&ch);
+    scanf("%f",&fvalue);
+    scanf("%s",&str);
+    printf("value is:%d",a);
+    printf("value is:%c",ch);
+    printf("value is:%f",fvalue);
+    printf("value is:%s",str);
+}
